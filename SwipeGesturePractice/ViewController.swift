cb942a24328a2b8670f87a5411b8b8553b2e05f0
Vincent Chang <vincentcch79@gmail.com>
2016-06-14 16:12:43 +0800

    afterclass practice one

--- SwipeGesturePractice/ViewController.swift
+++ SwipeGesturePractice/ViewController.swift
@@ -8,11 +8,23 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
+    
+     @IBOutlet weak var imageView: UIImageView!
+    
+    var currentnImageIndex: Int = 0
+    
+    let imageNames = ["black_concentrated.jpg", "black_crying.jpg", "black_dazed.jpg", "black_determined.jpg", "black_dude.jpg", "black_dumbfounded.jpg", "black_ewbte.jpg", "black_excited bliss.jpg", "black_excited tears.jpg", "black_excited.jpg"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        imageView.image = UIImage(named: "black_concentrated.jpg")
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +32,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func swipeRight(sender: AnyObject) {
+        print("swipeRight")
+        if currentnImageIndex < imageNames.count - 1 {
+            currentnImageIndex += 1
+        }
+        
+        if currentnImageIndex < imageNames.count  {
+            imageView.image = UIImage(named: imageNames[currentnImageIndex])
+        }
+//        
+//        if currentnImageIndex == 9 {
+//            currentnImageIndex = 0
+//        }
+        
+    }
+ 
+    @IBAction func swipeLeft(sender: AnyObject) {
+        print("swipeLeft")
+        
+        if currentnImageIndex > 0{
+            currentnImageIndex -= 1
+        }
+        if currentnImageIndex >= 0{
+            imageView.image = UIImage(named: imageNames[currentnImageIndex])
+        }
+        
+        
+        
+    }
 
 }
 
